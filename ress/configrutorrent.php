44d8ed338ded0ac2c7989e44d5b3bc930f654134
VoXaN24 <vali.so.518@gmail.com>
2020-04-18 21:46:51 +0200

    Add files via upload

---
+++ ress/configrutorrent.php
@@ -0,0 +1,12 @@
+$pathToExternals = array(
+    "curl"  => '/usr/bin/curl',
+    "stat"  => '/usr/bin/stat',
+    "php"    => '/usr/bin/php7.3',
+    "pgrep"  => '/usr/bin/pgrep',
+    "python" => '/usr/bin/python2.7'
+    );
+    
+$topDirectory = '/home/userseedbox';
+$scgi_port = 5001;
+$scgi_host = '127.0.0.1';
+$XMLRPCMountPoint = '/USERSEEDBOX';
