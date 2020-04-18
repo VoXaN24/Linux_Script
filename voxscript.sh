ca009cd95b1c4ff51a01337265dc90a34104e6ab
VoXaN24 <vali.so.518@gmail.com>
2020-04-18 15:36:09 +0200

    add Debian 8 to 9 script (EN only)

--- voxscript.sh
+++ voxscript.sh
@@ -11,36 +11,44 @@ if "$lang" == "en":
 	echo "Welcome in the textual interface of VoXaN24's scripts choices (curl requiered)"
 	echo "Nb Script: 2"
 	echo "1- Update Debian 8 to 10 (deb8to10.sh)"
-	echo "2- Install flood seedbox in Debian 10 (seedbox_deb10_flood.sh)"
-	echo "3- Update CentOS 7 to 8 (centos7to8.sh)"
-	echo "4- Update Debian 9 to 10 (deb9to10.sh)"
+	echo "2- Update Debian 9 to 10 (deb9to10.sh)"
+	echo "3- Update Debian 8 to 9 (deb8to9.sh)"
+	echo "4- Update CentOS 7 to 8 (centos7to8.sh)"
+	echo "5- Install flood seedbox in Debian 10 (seedbox_deb10_flood.sh)"
 	read -p "What's your choices ?" chx
 	if "$chx" == "1":
 		mkdir /tmp/vscript
 		curl https://voxan24.github.io/Linux_Script/deb8to10.sh -o /tmp/vscript/deb8t10.sh
 		chmod a+x /tmp/vscript/deb8t10.sh
-		bash /tmp/vscript/deb8t10
+		bash /tmp/vscript/deb8t10.sh
 		rm -r /tmp/vscript
 		exit
 	if "$chx" == "2":
 		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/seedbox_deb10_flood.sh -o /tmp/vscript/seedbox_deb10_flood.sh
-		chmod a+x /tmp/vscript/seedbox_deb10_flood.sh
-		bash /tmp/vscript/seedbox_deb10_flood.sh
+		curl https://voxan24.github.io/Linux_Script/deb9to10.sh -o /tmp/vscript/deb9t10.sh
+		chmod a+x /tmp/vscript/deb9t10.sh
+		bash /tmp/vscript/deb9t10.sh
 		rm -r /tmp/vscript
 		exit
 	if "$chx" == "3":
+		mkdir /tmp/vscript
+		curl https://voxan24.github.io/Linux_Script/deb9to10.sh -o /tmp/vscript/deb8to9.sh.sh
+		chmod a+x /tmp/vscript/deb8to9.sh
+		bash /tmp/vscript/deb8to9.sh
+		rm -r /tmp/vscript
+		exit
+	if "$chx" == "4":
 		mkdir /tmp/vscript
 		curl https://voxan24.github.io/Linux_Script/centos7to8.sh -o /tmp/vscript/centos7to8.sh
 		chmod a+x /tmp/vscript/centos7to8.sh
 		bash /tmp/vscript/centos7to8.sh
 		rm -r /tmp/vscript
 		exit
-	if "$chx" == "4":
+	if "$chx" == "5":
 		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb9to10.sh -o /tmp/vscript/deb9t10.sh
-		chmod a+x /tmp/vscript/deb9t10.sh
-		bash /tmp/vscript/deb9t10
+		curl https://voxan24.github.io/Linux_Script/seedbox_deb10_flood.sh -o /tmp/vscript/seedbox_deb10_flood.sh
+		chmod a+x /tmp/vscript/seedbox_deb10_flood.sh
+		bash /tmp/vscript/seedbox_deb10_flood.sh
 		rm -r /tmp/vscript
 		exit
 	else:
