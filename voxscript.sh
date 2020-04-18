c27a58dd45ec312a61a24c636d7a687dc5540eaa
VoXaN24 <vali.so.518@gmail.com>
2020-04-18 18:14:43 +0200

    Now it works ! (Normely)

--- voxscript.sh
+++ voxscript.sh
@@ -1,109 +1,134 @@
 #!/bin/bash
-#Script automatique 
-#Vérification sudo/root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 
-read -p "what's your language (en -> English, fr -> French)" lang
-if "$lang" == "en":
-	echo "Welcome in the textual interface of VoXaN24's scripts choices (curl requiered)"
-	echo "Nb Script: 5"
-	echo "1- Update Debian 8 to 10 (deb8to10.sh)"
-	echo "2- Update Debian 9 to 10 (deb9to10.sh)"
-	echo "3- Update Debian 8 to 9 (deb8to9.sh)"
-	echo "4- Update CentOS 7 to 8 (centos7to8.sh)"
-	echo "5- Install flood seedbox in Debian 10 (seedbox_deb10_flood.sh)"
-	read -p "What's your choices ?" chx
-	if "$chx" == "1":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb8to10.sh -o /tmp/vscript/deb8t10.sh
-		chmod a+x /tmp/vscript/deb8t10.sh
-		bash /tmp/vscript/deb8t10.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "2":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb9to10.sh -o /tmp/vscript/deb9t10.sh
-		chmod a+x /tmp/vscript/deb9t10.sh
-		bash /tmp/vscript/deb9t10.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "3":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb8to9.sh -o /tmp/vscript/deb8to9.sh.sh
-		chmod a+x /tmp/vscript/deb8to9.sh
-		bash /tmp/vscript/deb8to9.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "4":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/centos7to8.sh -o /tmp/vscript/centos7to8.sh
-		chmod a+x /tmp/vscript/centos7to8.sh
-		bash /tmp/vscript/centos7to8.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "5":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/seedbox_deb10_flood.sh -o /tmp/vscript/seedbox_deb10_flood.sh
-		chmod a+x /tmp/vscript/seedbox_deb10_flood.sh
-		bash /tmp/vscript/seedbox_deb10_flood.sh
-		rm -r /tmp/vscript
-		exit
-	else:
-		echo "Invalid, goodbye"
-		exit
-
-if "lang" == "fr":
-	echo "Bienvenue sur l'interface textuel de choix de Script du dépot de VoXaN24 (curl obligatoire)"
-	echo "Nombre de script disponible: 5"
-	echo "1- Script de mise a niveaux de Debian 8 vers Debian 10 (deb8to10.sh)"
-	echo "2- Script de mise a niveaux de Debian 9 vers Debian 10 (deb9to10.sh)"
-	echo "3- Script de mise a niveaux de Debian 8 vers Debian 8 (deb8to9.sh)"
-	echo "4- Script de mise a niveaux de CentOS 7 vers CentOS 8 (centos7to8.sh)"
-	echo "5- Script d'installation d'une seedbox avec flood sous Debian 10 (seedbox_deb10_flood.sh)"
-	
-	read -p "Quel est votre choix (entrez le chiffre correspondant a votre choix)" chx
-	if "$chx" == "1":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb8to10.sh -o /tmp/vscript/deb8t10.sh
-		chmod a+x /tmp/vscript/deb8t10.sh
-		bash /tmp/vscript/deb8t10.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "2":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb9to10.sh -o /tmp/vscript/deb9t10.sh
-		chmod a+x /tmp/vscript/deb9t10.sh
-		bash /tmp/vscript/deb9t10.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "3":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/deb8to9.sh -o /tmp/vscript/deb8to9.sh.sh
-		chmod a+x /tmp/vscript/deb8to9.sh
-		bash /tmp/vscript/deb8to9.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "4":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/centos7to8.sh -o /tmp/vscript/centos7to8.sh
-		chmod a+x /tmp/vscript/centos7to8.sh
-		bash /tmp/vscript/centos7to8.sh
-		rm -r /tmp/vscript
-		exit
-	if "$chx" == "5":
-		mkdir /tmp/vscript
-		curl https://voxan24.github.io/Linux_Script/seedbox_deb10_flood.sh -o /tmp/vscript/seedbox_deb10_flood.sh
-		chmod a+x /tmp/vscript/seedbox_deb10_flood.sh
-		bash /tmp/vscript/seedbox_deb10_flood.sh
-		rm -r /tmp/vscript
-		exit
-	else:
-		echo "Choix invalide, au revoirs"
-		exit	
-
-else:
-	echo "You didn't choise a valid language, goodbye my friends"
+function langchoise {
+	PS3="Your choise"
+	select item in "- English -" "- French -" "- Exit -"
+	do
+		echo "Vous avez choisi l'item $REPLY : $item"
+		case $REPLY in
+			1)
+				Eng
+				;;
+			2)
+				Fr
+				;;
+			3)
+				echo "Bye"
+				exit 0
+				;;
+			*)
+				echo "Wut ?"
+				;;
+		esac
+	done
+}
+function deb8to10 {
+	echo "Debian 8 to 10"
+	mkdir /tmp/vscript
+	curl https://voxan24.github.io/Linux_Script/deb8to10.sh -o /tmp/vscript/deb8to10.sh
+	chmod a+x /tmp/vscript/deb8to10.sh
+	bash /tmp/vscript/deb8to10.sh
+	rm -r /tmp/vscript
+	exit 0
+}
+function deb9to10{
+	echo "Debian 9 to 10"
+	mkdir /tmp/vscript
+	curl https://voxan24.github.io/Linux_Script/deb9to10.sh -o /tmp/vscript/deb9to10.sh
+	chmod a+x /tmp/vscript/deb9to10.sh
+	bash /tmp/vscript/deb9to10.sh
+	rm -r /tmp/vscript
+	exit 0
+}
+function deb8to9{
+	echo "Debian 8 to 9"
+	mkdir /tmp/vscript
+	curl https://voxan24.github.io/Linux_Script/deb8to9.sh -o /tmp/vscript/deb8to9.sh
+	chmod a+x /tmp/vscript/deb8to9.sh
+	bash /tmp/vscript/deb8to9.sh
+	rm -r /tmp/vscript
+	exit 0
+}
+function cos7to8{
+	echo "CentOS 7 to 8"
+	mkdir /tmp/vscript
+	curl https://voxan24.github.io/Linux_Script/centos7to8.sh -o /tmp/vscript/centos7to8.sh
+	chmod a+x /tmp/vscript/centos7to8.sh
+	bash /tmp/vscript/centos7to8.sh
+	rm -r /tmp/vscript
+	exit 0
+}
+function sbflooddeb10{
+	mkdir /tmp/vscript
+	curl https://voxan24.github.io/Linux_Script/seedbox_deb10_flood.sh -o /tmp/vscript/seedbox_deb10_flood.sh
+	chmod a+x /tmp/vscript/seedbox_deb10_flood.sh
+	bash /tmp/vscript/seedbox_deb10_flood.sh
+	rm -r /tmp/vscript
 	exit
+}
+function Eng{
+	PS3="Your choise"
+	select item in "- Debian 8 to 10 -" "- Debian 9 to 10 -" "- Debian 8 to 9 -" "- CentOS 7 to 8 -" "- Seedbox Flood for Debian 10 -" "- Exit -" "- Website -"
+	do
+		echo "Vous avez choisi l'item $REPLY : $item"
+		case $REPLY in
+			1)
+				deb8to10
+				;;
+			2)
+				deb9to10
+				;;
+			3)
+				deb8to9
+				;;
+			4)
+				cos7to8
+				;;
+			5)
+				sbflooddeb10
+				;;
+			6)
+				echo "Bye"
+				exit 0
+				;;
+			7)
+				echo "https://voxan24.github.io/Linux_Script/"
+				;;
+			*)
+				echo "Wut ?"
+				;;
+		esac
+	done
+}
+function Fr{
+	PS3="Votre choix "
+	select item in "- Debian 8 vers 10 -" "- Debian 9 vers 10 -" "- Debian 8 vers 9 -" "- CentOS 7 vers 8 -" "- Seedbox Flood pour Debian 10 -" "- Sortir -"
+	do
+		echo "Vous avez choisi l'item $REPLY : $item"
+		case $REPLY in
+			1)
+				deb8to10
+				;;
+			2)
+				deb9to10
+				;;
+			3)
+				deb8to9
+				;;
+			4)
+				cos7to8
+				;;
+			5)
+				sbflooddeb10
+				;;
+			6)
+				echo "Bye"
+				exit 0
+				;;
+			*)
+				echo "Quoi ?"
+				;;
+		esac
+	done
+}
+langchoise
