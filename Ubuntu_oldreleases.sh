488890843ffeca61bed9c4011414abe71dc62e16
Voxan24 <Tutute@gmail.com>
2020-10-25 11:01:49 +0100

    Update Ubuntu_oldreleases.sh
    5.04

--- Ubuntu_oldreleases.sh
+++ Ubuntu_oldreleases.sh
@@ -14,3 +14,17 @@ cd ~/ubuntu
 tar zcvf $ver.tar.gz dl/*
 mv $ver.tar.gz /var/www/html
 rm -rf *
+#Ubuntu 5.04
+ver=5.04
+mkdir dl
+cd dl
+wget http://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-install-i386.iso
+wget http://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-install-powerpc.iso
+wget http://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-install-amd64.iso
+wget http://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-live-i386.iso
+wget http://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-live-powerpc.iso
+wget http://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-live-amd64.iso
+cd ~/ubuntu
+tar zcvf $ver.tar.gz dl/*
+mv $ver.tar.gz /var/www/html
+rm -rf *
