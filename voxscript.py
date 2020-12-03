8b97dab79aa5da1eb3136186dd93a4644724afeb
Voxan24 <Tutute@gmail.com>
2020-12-03 16:10:39 +0100

    Update voxscript.py

--- voxscript.py
+++ voxscript.py
@@ -1,13 +1,2 @@
-def langue():
-	value=int(input("Pls write 1 for english, and 2 for french"))
-	if value==1:
-		eng()
-	elif value==2:
-		fr()
-	else:
-		langue()
-
-def eng():
-	print("Test")
-def fr():
-	print("Test but in french")
+def start():
+	
