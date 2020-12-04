50dc952a401dc3d9af0a49888e1e5bafd6eb485b
voxan24 <Lol@voxan24.cf>
2020-12-04 23:14:59 +0100

    Start of the ubuntu archive clone script
    This is useless .. Or not ?

---
+++ ubuntuarchiveclone.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo "First, pleases keep in mind, it takes like 1To to made a mirror for all supported ubuntu version"
+echo "Current Supported ubuntu version: bionic, focal, groovy, precise, trusty, xenial"
+echo "All file are downloaded from the official depot of ubuntu (the Canonical depot"
