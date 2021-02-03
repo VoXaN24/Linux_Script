380bb1cca490d9ab5d0aaf81cbf23c20604473a9
Voxan24 <Tutute@gmail.com>
2021-02-03 22:50:23 +0100

    Update fedora.sh

--- pcarchive/fedora.sh
+++ pcarchive/fedora.sh
@@ -100,3 +100,22 @@ wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/ppc/iso/F
 wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/ppc/iso/FC4-ppc-disc3.iso
 wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/ppc/iso/FC4-ppc-disc4.iso
 wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/ppc/iso/FC4-ppc-disc5.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-DVD.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-SRPMS-disc1.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-SRPMS-disc2.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-SRPMS-disc3.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-SRPMS-disc4.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-disc1.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-disc2.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-disc3.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-disc4.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-disc5.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/x86_64/iso/FC4-x86_64-rescuecd.iso
+cd ~/fedora
+tar zcvf $ver.tar.gz dl/*
+mv $ver.tar.gz /var/www/html
+rm -rf * 
+#Fedora core 4
+ver=4
+mkdir dl
+cd dl
