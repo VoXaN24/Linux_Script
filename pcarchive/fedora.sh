6f3f6adfa10d7655ed20830436be5d4d9b494cc3
Voxan24 <Tutute@gmail.com>
2021-02-03 22:46:56 +0100

    Update fedora.sh

--- pcarchive/fedora.sh
+++ pcarchive/fedora.sh
@@ -63,3 +63,30 @@ wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/i386/iso/
 wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/i386/iso/FC3-i386-disc2.iso
 wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/i386/iso/FC3-i386-disc4.iso
 wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/i386/iso/FC3-i386-rescuecd.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-DVD.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-SRPMS-disc1.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-SRPMS-disc2.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-SRPMS-disc3.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-SRPMS-disc4.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-disc1.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-disc2.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-disc3.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/3/x86_64/iso/FC3-x86_64-disc4.iso
+cd ~/fedora
+tar zcvf $ver.tar.gz dl/*
+mv $ver.tar.gz /var/www/html
+rm -rf * 
+#Fedora core 4
+ver=4
+mkdir dl
+cd dl
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-DVD.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-SRPMS-disc1.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-SRPMS-disc3.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-SRPMS-disc2.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-SRPMS-disc4.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-disc1.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-disc2.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-disc3.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-disc4.iso
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/core/4/i386/iso/FC4-i386-rescuecd.iso
