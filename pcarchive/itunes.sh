47e95036ef45574d3bacf13bc2296d3ec902d7d6
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2021-05-11 11:45:54 +0200

    Update itunes.sh

--- pcarchive/itunes.sh
+++ pcarchive/itunes.sh
@@ -3,46 +3,46 @@
 echo "Téléchargement de Itunes"
 mkdir Itunes
 cd Itunes
-mkdir 12.6.2
-cd 12.6.2
-wget https://api.ipsw.me/v4/itunes/download/windows/12.6.2?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.6.2?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.6.1
-cd 12.6.1
-wget https://api.ipsw.me/v4/itunes/download/windows/12.6.1?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.6.1?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.6
-cd 12.6
-wget https://api.ipsw.me/v4/itunes/download/windows/12.6?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.6?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.5.5
-cd 12.5.5
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.5?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.5?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.5.4
-cd 12.5.4
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.4?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.4?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.5.3
-cd 12.5.3
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.3?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.3?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.5.2
-cd 12.5.2
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.2?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.2?arch=x86 -O iTunesSetup.exe
-cd ..
-mkdir 12.5.1
-cd 12.5.1
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.1?arch=x64 -O iTunes64Setup.exe 
-wget https://api.ipsw.me/v4/itunes/download/windows/12.5.1?arch=x86 -O iTunesSetup.exe
-cd ..
+#mkdir 12.6.2
+#cd 12.6.2
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.6.2?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.6.2?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.6.1
+#cd 12.6.1
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.6.1?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.6.1?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.6
+#cd 12.6
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.6?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.6?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.5.5
+#cd 12.5.5
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.5?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.5?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.5.4
+#cd 12.5.4
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.4?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.4?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.5.3
+#cd 12.5.3
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.3?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.3?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.5.2
+#cd 12.5.2
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.2?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.2?arch=x86 -O iTunesSetup.exe
+#cd ..
+#mkdir 12.5.1
+#cd 12.5.1
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.1?arch=x64 -O iTunes64Setup.exe 
+#wget https://api.ipsw.me/v4/itunes/download/windows/12.5.1?arch=x86 -O iTunesSetup.exe
+#cd ..
 mkdir 12.4.3
 cd 12.4.3
 wget https://api.ipsw.me/v4/itunes/download/windows/12.4.3?arch=x64 -O iTunes64Setup.exe 
@@ -62,4 +62,184 @@ mkdir 12.4
 cd 12.4
 wget https://api.ipsw.me/v4/itunes/download/windows/12.4?arch=x64 -O iTunes64Setup.exe 
 wget https://api.ipsw.me/v4/itunes/download/windows/12.4?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.3.3
+cd 12.3.3
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.1.3
+cd 12.1.3
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.3.2
+cd 12.3.2
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.3.1
+cd 12.3.1
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.3
+cd 12.3
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.2.2
+cd 12.2.2
+wget https://api.ipsw.me/v4/itunes/download/windows/12.2.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.2.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.2.1
+cd 12.2.1
+wget https://api.ipsw.me/v4/itunes/download/windows/12.2.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.2.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.2
+cd 12.2
+wget https://api.ipsw.me/v4/itunes/download/windows/12.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.1.2
+cd 12.1.2
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.1.1
+cd 12.1.1
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.1
+cd 12.1
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 12.0.1
+cd 12.0.1
+wget https://api.ipsw.me/v4/itunes/download/windows/12.0.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/12.0.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.4
+cd 11.4
+wget https://api.ipsw.me/v4/itunes/download/windows/11.4?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.4?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.3.1
+cd 11.3.1
+wget https://api.ipsw.me/v4/itunes/download/windows/11.3.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.3.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.3
+cd 11.3
+wget https://api.ipsw.me/v4/itunes/download/windows/11.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.2.2
+cd 11.2.2
+wget https://api.ipsw.me/v4/itunes/download/windows/11.2.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.2.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.2.1
+cd 11.2.1
+wget https://api.ipsw.me/v4/itunes/download/windows/11.2.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.2.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.2
+cd 11.2
+wget https://api.ipsw.me/v4/itunes/download/windows/11.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.1.5
+cd 11.1.5
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.5?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.5?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.1.4
+cd 11.1.4
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.4?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.4?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.1.3
+cd 11.1.3
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.1.2
+cd 11.1.2
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.1.1
+cd 11.1.1
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.1
+cd 11.1
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.0.5
+cd 11.0.5
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.5?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.5?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.0.4
+cd 11.0.4
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.4?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.4?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.0.3
+cd 11.0.3
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.0.2
+cd 11.0.2
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.2?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.2?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11.0.1
+cd 11.0.1
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11.0.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 11
+cd 11
+wget https://api.ipsw.me/v4/itunes/download/windows/11?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/11?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 10.7
+cd 10.7
+wget https://api.ipsw.me/v4/itunes/download/windows/10.7?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/10.7?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 10.6.3
+cd 10.6.3
+wget https://api.ipsw.me/v4/itunes/download/windows/10.6.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/10.6.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 10.6.1
+cd 10.6.1
+wget https://api.ipsw.me/v4/itunes/download/windows/10.6.1?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/10.6.1?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 10.6
+cd 10.6
+wget https://api.ipsw.me/v4/itunes/download/windows/10.6?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/10.6?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 10.5.3
+cd 10.5.3
+wget https://api.ipsw.me/v4/itunes/download/windows/10.5.3?arch=x64 -O iTunes64Setup.exe 
+wget https://api.ipsw.me/v4/itunes/download/windows/10.5.3?arch=x86 -O iTunesSetup.exe
+cd ..
+mkdir 10.5.2
+cd 10.5.2
+wget https://secure-appldnld.apple.com/iTunes10/041-3448.20111212.3Ee34/iTunes64Setup.exe -O iTunes64Setup.exe 
+wget https://secure-appldnld.apple.com/iTunes10/041-3447.20111212.A1pY6/iTunesSetup.exe -O iTunesSetup.exe
 print 'end'
